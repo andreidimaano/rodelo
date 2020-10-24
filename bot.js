e7d7bc0cdf51b4742213d9fcd136f2bd9a9d07cc
Andrei Dimaano <andreiidimaano@gmail.com>
2020-10-24 16:02:05 -0700

    add command function

--- bot.js
+++ bot.js
@@ -1,5 +1,24 @@
+require('dotenv').config();
 let discord = require('discord.js');
 let client = new discord.Client();
-require('dotenv').config();
+let PREFIX = process.env.PREFIX;
+
 
 client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+});
+
+let isValidCommand = (message, cmdName) => message.content.toLowerCase().startsWith(PREFIX + cmdName);
+
+client.on('message', (message) => {
+    if(message.author.bot) return;
+    
+    if(isValidCommand(message, 'pomodoro')) {
+        message.channel.send(`${message.author} starting timer for 25 minutes, happy studying!`);
+    }
+
+    
+});
+
